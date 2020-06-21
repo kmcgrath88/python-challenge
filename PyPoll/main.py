5b3398a6369f60c24a7ec8982550c567891f7562
Kristi McGrath <mcgrathkristi@gmail.com>
2020-06-21 14:23:28 -0400

    Almost done

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,49 +1,45 @@
-#PYPOLL code for PYTHON CHALLENGE
+#-----PYPOLL code for PYTHON CHALLENGE-----#
 import os
 import csv
 
-#Variables/lists
-totalVotes = []
+totalVotes = [] #Empty list for total votes
 
+path = os.path.join("Resources", "PyPoll.csv") #Path to open csv
 
-#Path to open csv
-path = os.path.join("Resources", "PyPoll.csv")
-
-#Read csv file
-with open(path,"r") as text:
+with open(path,"r") as text: #Read csv file
     csvreader = csv.reader(text, delimiter=",")
+    
+    header = next(csvreader) #Skip header
 
-    #Skip header
-    header = next(csvreader)
-
-    #Loop through rows and collect votes and candidates
-    Dict1 = {}
-    for row in csvreader:
+    Dict1 = {} #Empty dictionary for candidates and vote counts
+    for row in csvreader: #Loop through rows and collect votes and candidates
         totalVotes.append(row[0])
-        candidate, votes = row[2],1   
-        if candidate not in Dict1:
-            Dict1[candidate] = votes     
+        candidate, votes = row[2], 1 #Variables for dictionary
+        if candidate not in Dict1: #If candidate not in dictionary, add to dictionary
+            Dict1[candidate] = votes #????     
         else:
             Dict1[candidate] +=1
-WIN = max(Dict1, key=Dict1.get)
+WIN = max(Dict1, key=Dict1.get) #??? #Find winner
 
+#-----Print results to terminal-----#
 print(f'Election Results')
-print(f'----------------------------')
+print(f'--------------------------')
 print(f'Total Votes: {len(totalVotes)}')
-for candidate in Dict1:
+for candidate in Dict1: #Loop through dictionay, print candidates, calculate percent of votes
     print(f'{candidate}: {(Dict1[candidate]/(len(totalVotes))*100):.3f}% ({Dict1[candidate]})')
-print(f'----------------------------')       
+print(f'--------------------------')       
 print(f'Winner: {WIN}')
 
-outPATH = os.path.join("Analysis", "TestPath2.txt")
+outPATH = os.path.join("Analysis", "Election Results.txt") #Outpath to create text file
+#-----Print results to text file-----#
 with open(outPATH, "w") as text2:
 
     text2.write(f'Election Results\n')
-    text2.write(f'----------------------------\n')
+    text2.write(f'--------------------------\n')
     text2.write(f'Total Votes: {len(totalVotes)}\n')
-    text2.write(f'----------------------------\n')
+    text2.write(f'--------------------------\n')
     for candidate in Dict1: 
         text2.write(f'{candidate}: {(Dict1[candidate]/(len(totalVotes))*100):.3f}% ({Dict1[candidate]})\n') 
-    text2.write(f'----------------------------\n')
+    text2.write(f'--------------------------\n')
     text2.write(f'Winner: {WIN}\n')
-    text2.write(f'----------------------------')
+    text2.write(f'--------------------------')
